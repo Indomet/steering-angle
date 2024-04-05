8b917b0087071ff9fc1c8db5b8b963320b37decc
zepei <zepeiz2004@gmail.com>
2024-04-05 16:35:44 +0200

    Add failing test case

--- src/PrimeChecker.cpp
+++ src/PrimeChecker.cpp
@@ -1,14 +1,12 @@
 #include "PrimeChecker.hpp"
 
 bool PrimeChecker::isPrime(uint16_t n) {
-    bool retVal{true};
-    if (n<2 || 0 == n%2) {
+    bool retVal { true };
+    if (n < 2 || 0 == n % 2) {
         retVal = false;
-    }
-    else {
-        for(uint16_t i{3}; (i*i) <= n; i += 2) {
-            // this is a useful mutation to fail the test in assignment 8
-            if (0 != n%i) {
+    } else {
+        for (uint16_t i { 3 }; (i * i) <= n; i += 2) {
+            if (0 == n % i) {
                 return false;
                 break;
             }
--- src/TestPrimeChecker.cpp
+++ src/TestPrimeChecker.cpp
@@ -1,9 +1,15 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this once per test-runner!
+#define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this once per test-runner!
 
-#include "catch.hpp"
 #include "PrimeChecker.hpp"
+#include "catch.hpp"
 
 TEST_CASE("Test PrimeChecker 1.") {
     PrimeChecker pc;
     REQUIRE(pc.isPrime(5));
+}
+
+// This test case should fail
+TEST_CASE("Test PrimeChecker 2.") {
+    PrimeChecker pc;
+    REQUIRE(pc.isPrime(4));
 }
