48fa98cb665bfc6f4da3108d8ec61ea161c44409
almuslim <almuslim@student.chalmers.se>
2024-04-24 20:59:45 +0200

    move cone detector to another file

--- src/template-opencv.cpp
+++ src/template-opencv.cpp
@@ -17,7 +17,6 @@
 
 // Include the single-file, header-only middleware libcluon to create high-performance microservices
 #include "cluon-complete.hpp"
-#include <chrono>
 // #include <detect-cones.hpp>
 //  Include the OpenDLV Standard Message Set that contains messages that are usually exchanged for automotive or robotic applications
 #include "opendlv-standard-message-set.hpp"
@@ -26,12 +25,17 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <vector>
+#include <cone_detection/cone_detector.hpp>
+
+/*
 cv::Mat detect_cones(cv::Mat& img);
 cv::Mat get_roi(cv::Mat& img);
 cv::Mat get_hsv(cv::Mat& img,cv::Scalar lower_bounds, cv::Scalar upper_bounds);
 cv::Mat find_conts(cv::Mat& hsv_roi_img,cv::Mat& og_img);
 
 #define LEN_CONES 2
+#define Y_START 0.55
+#define Y_END 0.23
 
 const cv::Scalar LOWER_BLUE = cv::Scalar(88,76,0);
 const cv::Scalar HIGHER_BLUE = cv::Scalar(141,255,255);
@@ -40,7 +44,7 @@ const cv::Scalar HIGHER_YELLOW = cv::Scalar(30,255,255);
 
 
 const std::vector <cv::Scalar> LOWER_BOUNDS = {LOWER_BLUE,LOWER_YELLOW};
-const std::vector <cv::Scalar> UPPER_BOUNDS = {HIGHER_BLUE,HIGHER_YELLOW};
+const std::vector <cv::Scalar> UPPER_BOUNDS = {HIGHER_BLUE,HIGHER_YELLOW};*/
 
 int32_t main(int32_t argc, char** argv) {
     int32_t retCode { 1 };
@@ -120,12 +124,13 @@ int32_t main(int32_t argc, char** argv) {
 
     return retCode;
 }
-
+/*
 
 cv::Mat detect_cones(cv::Mat& img){
-
-    for (int i=0; i < LEN_CONES; i++){
-        cv::Mat roi = get_roi(img);
+    cv::Mat roi = get_roi(img);
+    return roi;
+    //return get_hsv(roi,LOWER_BOUNDS.at(i),UPPER_BOUNDS.at(i));
+    /*for (int i=0; i < LEN_CONES; i++){
         cv::Mat hsv = get_hsv(roi,LOWER_BOUNDS.at(i),UPPER_BOUNDS.at(i));
         img = find_conts(hsv,img);
     }
@@ -133,7 +138,7 @@ cv::Mat detect_cones(cv::Mat& img){
 
 }
 cv::Mat get_roi(cv::Mat& img){
-    cv::Mat region (img, cv::Rect(0,(int)img.rows*0.55,img.cols,(int)img.rows*0.28));
+    cv::Mat region (img, cv::Rect(0,(int)img.rows*Y_START,img.cols,(int)img.rows*Y_END));
     return region;
 }
 
@@ -160,11 +165,11 @@ cv::Mat find_conts(cv::Mat& hsv_roi_img,cv::Mat& og_img){
     for(size_t  i=0; i< contours.size(); i++){
         cv::Rect bounding_rect = cv::boundingRect(contours[i]);
         //add back the cropped image height to the y coordinate
-        bounding_rect.y += (int)og_img.rows*0.5;
+        bounding_rect.y += (int)og_img.rows*Y_START;
 
         cv::rectangle(og_img,bounding_rect,cv::Scalar(0,0,255),2,cv::LINE_8);
         
     }
     return og_img;
     
-}
+}*/
